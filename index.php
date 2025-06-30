351fdb8fdb0ccb398c4334d0152f4b6e241b4421
Vitya <bb.whitew@mail.ru>
2025-06-30 23:40:43 +0300

    IH8JQUERY

--- index.php
+++ index.php
@@ -8,20 +8,20 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Database\Init;
 use FileCheck\FileChecker;
 
-// try {
-//     $init = new Init('mysql');
+try {
+    $init = new Init('mysql');
 
-//     $result = $init->get();
+    $result = $init->get();
 
-//     if ($result) {
-//         print_r($result);
-//     } else {
-//         echo "Нет данных";
-//     }
+    if ($result) {
+        print_r($result);
+    } else {
+        echo "Нет данных";
+    }
 
-// } catch (Throwable $t) {
-//     echo "Ошибка: " . $t->getMessage();
-// }
+} catch (Throwable $t) {
+    echo "Ошибка: " . $t->getMessage();
+}
 
 $cheker = new FileChecker();
 
